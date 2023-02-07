9b10acdf459a544484c44a9d75e767104f831bc5
Protyush Nayak <46063095+protyushn1999@users.noreply.github.com>
2023-02-07 16:59:25 +0530

    Add files via upload

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wqjgwq6f',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/index.js
@@ -0,0 +1,8 @@
+// export testimonial schema
+import testimonial from './testimonial'
+import contact from './contact'
+import skills from './skills'
+import works from './works'
+import experiences from './experiences'
+import workexperiences from './workexperiences'
+export const schemaTypes = [testimonial,contact,skills,works,experiences,workexperiences]
